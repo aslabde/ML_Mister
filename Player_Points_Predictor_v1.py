57621331d5f2d2a5add2260714fcc2c039464eb5
Eduardo Balsa <aslabde@gmail.com>
2024-05-07 00:57:15 +0200

    Model working, but need tunning on h-param

--- Player_Points_Predictor_v1.py
+++ Player_Points_Predictor_v1.py
@@ -3,26 +3,33 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from xgboost import XGBRegressor
 
 
 # Load the CSV file into a DataFrame
-df = pd.read_excel('Embarba_J33.xlsx')
+df = pd.read_excel('Embarba_J34.xlsx')
 
 df['Lag_1'] = df['POINTS'].shift(1)
 df['Lag_2'] = df['POINTS'].shift(2)
-df = df.reindex(columns=['GAME','POINTS', 'Lag_1', 'Lag_2'])
+df['Lag_3'] = df['POINTS'].shift(3)
+df['Lag_4'] = df['POINTS'].shift(4)
+df = df.reindex(columns=['GAME','POINTS', 'Lag_1', 'Lag_2', 'Lag_3', 'Lag_4'])
 
-# Display the first few rows of the DataFrame
-print(df.head())
+df = df.dropna(axis=0)
 
-y = df['POINTS']
-X = df.fillna(0)
+y = df.POINTS
+X = df.reindex(columns=['GAME', 'Lag_1', 'Lag_2', 'Lag_3', 'Lag_4'])
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, shuffle=False)
-model = LinearRegression()
+
+model = XGBRegressor()
 model.fit(X_train, y_train)
 
-y_fit = pd.DataFrame(model.predict(X_train), index=X_train.index, columns=y.columns)
-y_pred = pd.DataFrame(model.predict(X_test), index=X_test.index, columns=y.columns)
+y_fit = pd.DataFrame(model.predict(X_train))
+y_pred = pd.DataFrame(model.predict(X_test))
+
+print(X_test)
+print(y_pred.round())
 
 '''
 fig, ax = plt.subplots()
